7c6d092112625b76767009ba3467454e9b84f403
kondratskyivlad <55400806+kondratskyivlad@users.noreply.github.com>
2020-10-23 17:54:29 +0300

    add all files and changes

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "calculator.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	Calculator calc;
+	cout<<calc.Add(654.0, 321.0)<<endl;
+
+	return 0;
+}
